be715d657fa9399eb41bb2d74d77f7ea8af9dfe2
Zuzanna Zajkiewicz <machefi1@gmail.com>
2020-11-29 20:58:40 +0100

    Correct src/index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get("/", function(request, response){
-    response.send("Hello world!");
+    response.send("Hello World!");
 })
 
 
